c2d6eabd2b4d8c210d2f2b59697a064537dd530e
sohilasalah16 <120594461+sohilasalah16@users.noreply.github.com>
2025-10-05 18:22:26 +0300

    update my branch before push

--- ExaminationSystem/Program.cs
+++ ExaminationSystem/Program.cs
@@ -13,7 +13,7 @@ namespace ExaminationSystem
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new UserForm());
+            Application.Run(new FormBase());
         }
     }
 }
