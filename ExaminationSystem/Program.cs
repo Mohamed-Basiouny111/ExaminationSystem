cd7f649d0438994d35036b36c5eb693fbec13d0d
Mohamed Basiouny <mohamedbasiouny111@gmail.com>
2025-10-08 17:39:28 +0300

    Some Edit

--- ExaminationSystem/Program.cs
+++ ExaminationSystem/Program.cs
@@ -18,15 +18,7 @@ namespace ExaminationSystem
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-
-            Application.Run(new ChooseAllQuestionForm());
-            //Application.Run(new UserForm());
-            //Application.Run(new LoginForm());
-
-            //Application.Run(new teacherExam());
-
-           // Application.Run(new ExamForm2(1, 3));
+            ApplicationConfiguration.Initialize();           
            
             Application.Run(new LoginForm());
 
